1f70f3bb205a542bf79290d34df3936e33fc902b
MaxGomas <mx.gomas@gmail.com>
2022-01-26 15:12:42 +0100

    Commit made from VS Code Azure Static Web Apps

--- api/products-get/index.js
+++ api/products-get/index.js
@@ -5,7 +5,7 @@ module.exports = async function (context, req) {
 
     try {
         const products = data.getProducts();
-        context.res.status(201).json(products);
+        context.res.status(200).json(products);
       } catch (error) {
         context.res.status(500).send(error);
       }
